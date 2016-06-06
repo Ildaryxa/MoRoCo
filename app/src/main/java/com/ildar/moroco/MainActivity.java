8cc21fc67fd5a4529fe509ca9097f7b0648f76b3
Ildar <ildaryxa@gmail.com>
2016-06-07 00:23:32 +0500

    проверка на наличие bluetooth и его включение, переход на навое Activity

--- app/src/main/java/com/ildar/moroco/MainActivity.java
+++ app/src/main/java/com/ildar/moroco/MainActivity.java
@@ -1,13 +1,45 @@
 package com.ildar.moroco;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    final int REQUEST_ENABLED_BLUETOOTH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnConnection(View view) {
+        BluetoothAdapter bluetooth= BluetoothAdapter.getDefaultAdapter();
+        if(bluetooth!=null)
+        {
+            if (!bluetooth.isEnabled()){
+                //значит bluetooth выключен, попросить пользователя включить его
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLED_BLUETOOTH);
+            }
+        } else {
+            //bluetooth модуля не сущесвует
+            Toast.makeText(this, "На данном устройстве нет Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK){
+            Intent intent = new Intent(this, DeviceSelection.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Для дальнейшей работы необходимо Bluetooth соединение!", Toast.LENGTH_LONG).show();
+        }
     }
 }
