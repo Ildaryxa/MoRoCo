4de18773f4913c69069f089b8d11a650f9556ae3
Ildar <ildaryxa@gmail.com>
2016-06-28 15:55:17 +0500

    лишний кож

--- app/src/main/java/com/ildar/moroco/MainActivity.java
+++ app/src/main/java/com/ildar/moroco/MainActivity.java
@@ -73,38 +73,6 @@ public class MainActivity extends Activity {
         return connected;
     }
 
-    //region лишний код
-    /*
-    public void btnConnection(View view) {
-        BluetoothAdapter bluetooth= BluetoothAdapter.getDefaultAdapter();
-        if(bluetooth!=null)
-        {
-            if (!bluetooth.isEnabled()){
-                //значит bluetooth выключен, попросить пользователя включить его
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLED_BLUETOOTH);
-            }else {
-                createActivity();
-            }
-
-        } else {
-            //bluetooth модуля не сущесвует
-            Toast.makeText(this, "На данном устройстве нет Bluetooth", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (resultCode == RESULT_OK){
-            createActivity();
-        }
-        else{
-            Toast.makeText(this, "Для дальнейшей работы необходимо Bluetooth соединение!", Toast.LENGTH_LONG).show();
-        }
-    }
-    */
-    //endregion
 
     public void onCreateActivity(final View view){
         progressDialog = ProgressDialog.show(this, "Поиск устроиств", "Подождите...");
